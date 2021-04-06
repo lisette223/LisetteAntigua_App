0446b28bd42117bead37d2d830cb21532d8fec60
LisetteAntigua <antigua223@gmail.com>
2021-04-05 20:40:29 -0400

    AR Kit Implementation

---
+++ LisetteAntigua_App/Scientist.swift
@@ -0,0 +1,17 @@
+//
+//  Scientist.swift
+//  LisetteAntigua_App
+//
+//  Created by Lisette Antigua on 4/5/21.
+//
+
+import Foundation
+
+struct Scientist: Decodable{
+    let name: String
+    let dates: String
+    let field: String
+    let bio: String
+    let country: String
+    let source: String
+}
--- LisetteAntigua_App/ViewController.swift
+++ LisetteAntigua_App/ViewController.swift
@@ -12,29 +12,27 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var scientists =[String, Scientist]()
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        let configuration = ARImageTrackingConfiguration()
+        
+        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "scientists", bundle: nil) else{
+            fatalError("Couldn't load tracking images")
+        }
+        configuration.trackingImages = trackingImages
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,29 +44,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let imageAnchor = anchor as? ARImageAnchor else{ return nil}
+        
+        let plane  = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        plane.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi/2
         let node = SCNNode()
-     
+        node.addChildNode(planeNode)
         return node
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
 }
